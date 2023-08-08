1a0479910df0ca8b2f43ba8512dada62b5cc127f
Sridhar <sridharonstar@gmail.com>
2023-08-08 13:20:51 +0530

    cart update

--- lib/data/controllers/cart_controller.dart
+++ lib/data/controllers/cart_controller.dart
@@ -1,3 +1,9 @@
-class CartRepo{
-  
+import 'package:get/get.dart';
+import '../../models/cart.dart';
+import '../repository/cart_repo.dart';
+
+class CartController extends GetxController{
+  final CartRepo cartRepo;
+  CartController({required this.cartRepo});
+  Map<int,CartModel> items={};
 }
